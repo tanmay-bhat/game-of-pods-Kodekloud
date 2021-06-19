e1979445ffe75d3e17412a7e95794828a89735e1
Tanmay Bhat <>
2021-06-19 23:35:18 +0530

    add fileserver deployment and patch script

---
+++ Pento/fix_cluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+==============================================
+echo "Fixing kubeconfig file.........."
+echo
+cp /etc/kubernetes/admin.conf ~/.kube/config
+export KUBECONFIG=~/.kube/config
+echo
+echo "Fixing api server by updating manifest file........"
+sed  -i 's/ca-authority.crt/ca.crt/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+echo
+echo "Fixing coredns image..........."
+echo
+kubectl set image deployment/coredns -n kube-system coredns=k8s.gcr.io/coredns:1.3.1
+echo
+echo "Uncordoning worker node........"
+echo
+kubectl uncordon node01
+echo
+kubectl get nodes
+ssh node01 && mkdir /web && logout
+==================================================
