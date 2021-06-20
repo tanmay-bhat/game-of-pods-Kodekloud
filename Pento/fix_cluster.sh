2bfc34ddd702c79d746fcb178125f8d850f020f0
Tanmay Bhat <>
2021-06-20 12:20:35 +0530

    fix pvc

--- Pento/fix_cluster.sh
+++ Pento/fix_cluster.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-==============================================
+echo "=============================================="
 echo "Fixing kubeconfig file.........."
 echo
 cp /etc/kubernetes/admin.conf ~/.kube/config
 export KUBECONFIG=~/.kube/config
-echo
 echo "Fixing api server by updating manifest file........"
 sed  -i 's/ca-authority.crt/ca.crt/g' /etc/kubernetes/manifests/kube-apiserver.yaml
 echo
 echo "Fixing coredns image..........."
-echo
-kubectl set image deployment/coredns -n kube-system coredns=k8s.gcr.io/coredns:1.3.1  2 > /dev/null
+kubectl set image deployment/coredns coredns=k8s.gcr.io/coredns:1.3.1 -n kube-system 2> /dev/null
 echo
 echo "Uncordoning worker node........"
 echo
-kubectl uncordon node01
-echo
-kubectl get nodes
-ssh node01 && mkdir /web 
-echo " type 'exit to logout from noe01"
-==================================================
+kubectl uncordon node01 
+echo "================================================"
