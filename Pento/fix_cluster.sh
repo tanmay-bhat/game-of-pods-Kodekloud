6fca71f35fd2f481df26b30b283efe4ab3e4cbe4
Tanmay Bhat <>
2021-06-20 13:22:14 +0530

    final fix

--- Pento/fix_cluster.sh
+++ Pento/fix_cluster.sh
@@ -3,14 +3,21 @@ echo "=============================================="
 echo "Fixing kubeconfig file.........."
 echo
 cp /etc/kubernetes/admin.conf ~/.kube/config
-export KUBECONFIG=~/.kube/config
+#export KUBECONFIG=~/.kube/config
 echo "Fixing api server by updating manifest file........"
-sed  -i 's/ca-authority.crt/ca.crt/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed  -i 's/ca-authority.crt/ca.crt/g' /etc/kubernetes/manifests/kube-apiserver.yaml 2>/dev/null
+echo
+sleep 3
+kubectl get pods -A
+sleep 3
 echo
 echo "Fixing coredns image..........."
-kubectl set image deployment/coredns coredns=k8s.gcr.io/coredns:1.3.1 -n kube-system 2> /dev/null
+kubectl set image deployment.apps/coredns coredns=k8s.gcr.io/coredns:1.3.1 -n kube-system 2>/dev/null
+echo
+echo "uppdated coredns Image:"
+kubectl describe deploy coredns -n kube-system | grep -i image
 echo
 echo "Uncordoning worker node........"
 echo
-kubectl uncordon node01 
+kubectl uncordon node01 2>/dev/null
 echo "================================================"
---
+++ drupal/readme.md
@@ -0,0 +1,9 @@
+Step #1
+ create a directory /drupal-data on WN
+
+ mkdir /drupal-data
+
+Step #2
+Create the pv : drupal-pv by below command :
+
+kubectl apply -f pv-create.yaml
